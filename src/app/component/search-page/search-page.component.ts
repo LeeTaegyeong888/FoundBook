6a97f1154850fcb84a6178f557ac5f56bdf31801
tklee <vitkwow000000@gmail.com>
2021-11-24 18:30:37 +0900

    GUI 완성, 책검색 클릭시 카카오검색화면으로 이동

--- src/app/component/search-page/search-page.component.ts
+++ src/app/component/search-page/search-page.component.ts
@@ -5,8 +5,21 @@ import { Router } from '@angular/router';
 import {
   bookDataContent,
   bookServerData,
-  metaServerData,
 } from 'src/app/service/book/book-interface';
+import { filter } from 'rxjs/operators';
+
+interface bookPropertyData {
+  title: string;
+  authors: string;
+  translators: string;
+  publisher: string;
+  datetime: string;
+  contents: string;
+  url: string;
+  thumbnail: string;
+  sale_price: number;
+  price: number;
+}
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
@@ -15,8 +28,9 @@ import {
 export class SearchPageComponent implements OnInit {
   private searchSub: Subscription;
 
-  public bookData: bookDataContent[] = [];
+  public bookData: bookPropertyData[] = [];
   public isMoreBookData: boolean = false;
+  public isLoad: boolean = false;
   @ViewChild('menuField', { read: ElementRef, static: false })
   menuFieldEle!: ElementRef;
   constructor(private BookService: BookService, private router: Router) {
@@ -29,12 +43,14 @@ export class SearchPageComponent implements OnInit {
 
   ngOnDestroy(): void {
     if (this.searchSub) this.searchSub.unsubscribe();
+    this.isLoad = false;
   }
 
   onDataChanged(searchData: bookServerData): void {
     if (!searchData) return;
     console.log('SearchPageComponent  search Data :: ', searchData);
-    this.bookData = searchData.documents;
+    //this.bookData = searchData.documents;
+    this.parseBookData(searchData.documents);
     this.isMoreBookData = !searchData.meta.is_end;
   }
 
@@ -45,6 +61,40 @@ export class SearchPageComponent implements OnInit {
 
   onMenuClick() {
     console.log('onMenuClick');
-    this.menuFieldEle.nativeElement.classList.add('menuFieldFocus');
+    if (this.menuFieldEle.nativeElement.classList.contains('menuFieldFocus')) {
+      this.menuFieldEle.nativeElement.classList.remove('menuFieldFocus');
+    } else {
+      this.menuFieldEle.nativeElement.classList.add('menuFieldFocus');
+    }
+  }
+
+  goToBookLink(url: string) {
+    console.log('goToBookLink');
+    window.open(url, '_blank');
+  }
+
+  private parseBookData(bookDatas: bookDataContent[]) {
+    this.bookData = bookDatas
+      .filter((books: bookDataContent) => {
+        return books.status !== '';
+      })
+      .map((book: bookDataContent) => {
+        return {
+          title: book.title,
+          authors: book.authors.join(', '),
+          translators: book.translators.join(', '),
+          publisher: book.publisher,
+          datetime: book.datetime.split('T')[0],
+          contents: book.contents,
+          url: book.url,
+          thumbnail: book.thumbnail,
+          sale_price: book.sale_price,
+          price: book.price,
+        };
+      });
+
+    this.isLoad = true;
+
+    console.log('parseBookData  this.bookData :: ', this.bookData);
   }
 }
--- src/app/service/book/book-interface.ts
+++ src/app/service/book/book-interface.ts
@@ -1,23 +1,23 @@
 export interface bookDataContent {
-    authors: string[];
-    contents: string;
-    datetime: string;
-    price: number;
-    publisher: string;
-    sale_price: number;
-    status: string;
-    thumbnail: string;
-    title: string;
-    translators: string[];
-    url: string;
-    isbn: string;
+  authors: string[];
+  contents: string;
+  datetime: string;
+  price: number;
+  publisher: string;
+  sale_price: number;
+  status: string;
+  thumbnail: string;
+  title: string;
+  translators: string[];
+  url: string;
+  isbn: string;
 }
 export interface metaServerData {
-    is_end: boolean;
-    pageable_count: number;
-    total_count: number;
+  is_end: boolean;
+  pageable_count: number;
+  total_count: number;
 }
 export interface bookServerData {
-    documents: bookDataContent[];
-    meta: metaServerData
+  documents: bookDataContent[];
+  meta: metaServerData;
 }
